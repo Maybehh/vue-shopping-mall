fba14bf113025a34c8b48d2f83e4c964eaa7add6
Maybehh <1280025619@qq.com>
2021-02-25 15:54:12 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import app from './App.vue'
+import { Header } from 'mint-ui'
+import './lib/mui/css/mui.min.css'
+
+Vue.config.devtools = true
+
+Vue.component(Header.name, Header)
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
